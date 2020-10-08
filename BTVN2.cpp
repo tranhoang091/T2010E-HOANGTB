9247c615c131c208d1739f9322d754abb0f30078
tranhoang091 <72307260+tranhoang091@users.noreply.github.com>
2020-10-08 17:59:46 +0700

    BTVN

---
+++ BTVN2.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+	int a;
+	printf("nhap a=");
+	scanf("%d",&a);
+	int b;
+	printf("nhap b=");
+	scanf("%d",&b);
+	if(a>=b){
+		printf("thi a/b");
+	}else{
+		printf("thi a*b");
+	}
+	}
+	
+
